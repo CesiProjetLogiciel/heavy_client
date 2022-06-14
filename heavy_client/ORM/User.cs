4f285f4e2f28c4223dab4cc73bdbbfe2f4ee29ab
Amaury CHABANE <amaurychabane@gmail.com>
2022-06-14 17:00:02 +0200

    Created User ORM file

---
+++ heavy_client/ORM/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heavy_client.ORM
+{
+    public class User : INotifyPropertyChanged
+    {
+        public int UserID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string email { get; set; }
+        public bool isSuspended { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserType { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
