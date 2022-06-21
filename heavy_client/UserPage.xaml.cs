044290180713c4665d2b953f628582509601e1d8
Amaury CHABANE <amaurychabane@gmail.com>
2022-06-21 09:22:09 +0200

    Fixed UserSelected is null Exception

--- heavy_client/UserPage.xaml.cs
+++ heavy_client/UserPage.xaml.cs
@@ -60,6 +60,13 @@ namespace heavy_client
         public UserPage()
         {
             InitializeComponent();
+        }
+
+        // Define this method within your main page class.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            UserSelected = e.Parameter as User;
 
             if (!UserPermissions.IsAllowed("DeliveryAddress", "INSERT"))
                 AddDeliveryAddress.IsEnabled = false;
@@ -74,7 +81,7 @@ namespace heavy_client
                 ClientTypeComboBox.IsEnabled = false;
             }
 
-            if (!(UserPermissions.IsAllowed("paypalAddress", "UPDATE") 
+            if (!(UserPermissions.IsAllowed("paypalAddress", "UPDATE")
                 && UserPermissions.IsAllowed("paypalAddress", "INSERT")))
                 PaypalEmailTextBox.IsEnabled = false;
 
@@ -87,13 +94,6 @@ namespace heavy_client
                     && UserPermissions.IsAllowed("paypalAddress", "INSERT")
                     && _isPaypalAllowed.Contains(UserSelected.UserType.Type.ToLower())))
                 Save_Button.IsEnabled = false;
-        }
-
-        // Define this method within your main page class.
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-            UserSelected = e.Parameter as User;
 
             // Instead of hard coded items, the data could be pulled
             // asynchronously from a database or the internet.
